d280bbf401c503e4cc7eb8173d6c581d7b2cbbdd
Robotsi <alperenemrekir52@gmail.com>
2024-09-29 18:04:59 +0300

    Organizasyon ve Kişiler arası yetkilendirme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { ConvexClientProvider } from './ConvexClientProvider';
 import './globals.css';
+import { Header } from './header';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Header />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,41 @@
 'use client';
 import { Button } from '@/components/ui/button';
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-} from '@clerk/nextjs';
+import { useOrganization, useUser } from '@clerk/nextjs';
 import { useMutation, useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 
 export default function Home() {
+  const organization = useOrganization();
+  const user = useUser();
+  let orgId: string | undefined = undefined;
+  if (organization.isLoaded && user.isLoaded) {
+    orgId = organization.organization?.id ?? user.user?.id;
+  }
   const createFiles = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : 'skip');
   return (
     <main className='flex justify-center items-center'>
-      <SignedIn>
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+
+      <Button
+        onClick={() => {
+          if (!orgId) return;
+          createFiles({
+            name: 'Hello world',
+            orgId,
+          });
+        }}
+      >
+        Click me
+      </Button>
+    </main>
+  );
+}
+
+/*
+<SignedIn>
         <SignOutButton>
           <Button>Sign Out</Button>
         </SignOutButton>
@@ -24,20 +45,4 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
-
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>;
-      })}
-
-      <Button
-        onClick={() => {
-          createFiles({
-            name: 'Hello world',
-          });
-        }}
-      >
-        Click me
-      </Button>
-    </main>
-  );
-}
+*/
